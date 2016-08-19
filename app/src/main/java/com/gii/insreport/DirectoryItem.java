50c22fb41af301cd98538806bb977b9a71cbe8d4
Timur Bakibayev <Timur Bakibayev>
2016-08-19 09:46:43 +0600

    missing files

---
+++ app/src/main/java/com/gii/insreport/DirectoryItem.java
@@ -0,0 +1,22 @@
+package com.gii.insreport;
+
+/**
+ * Created by Timur_hnimdvi on 16-Aug-16.
+ */
+public class DirectoryItem {
+    public String id = "";
+    public String name = "";
+    public boolean status = true;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+}
