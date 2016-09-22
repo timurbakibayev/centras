8a4e85bde567393f8054d1983b3d66fcde597c44
Acer <Acer>
2016-09-23 02:37:50 +0600

    PreInsurance form

---
+++ app/src/main/java/com/gii/insreport/PreInsuranceActivity.java
@@ -0,0 +1,13 @@
+package com.gii.insreport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreInsuranceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_insurance);
+    }
+}
